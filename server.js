307a074759b67ab5c03af007c2148601b936a756
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-10-10 17:50:18 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const{DB_HOST, PORT =3000} = process.env;
+const{DB_HOST, PORT} = process.env;
+console.log(DB_HOST, PORT)
 
 mongoose.connect(DB_HOST)
   .then(() => {
@@ -14,3 +15,6 @@ mongoose.connect(DB_HOST)
     console.log(error.message);
     process.exit(1);
   })
+
+  // DB_HOST=mongodb+srv://Tamara:2t3aTJvXwi3WnHjI@cluster0.2mtshu6.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=AtlasAppv
+  // PORT=3000
