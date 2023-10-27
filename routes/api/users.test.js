e90273a1da5192117f1805a01aaa78d143595071
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-10-27 12:56:22 +0300

    test

--- routes/api/users.test.js
+++ routes/api/users.test.js
@@ -3,7 +3,6 @@ const app = require('../../app');
 const request = require('supertest');
 const { User } = require('../../models/User');
 
-
 const { TEST_DB_HOST, PORT = 3000 } = process.env;
 
 describe("test login route", () => {
@@ -17,25 +16,25 @@ describe("test login route", () => {
         await mongoose.connection.close();
         server.close();
     })
-        afterEach(async () => {
-        await User.deleteMany({});
-    })
+    //     afterEach(async () => {
+    //     await User.deleteMany({});
+    // })
 
-    test("test register with correct data", async () => {
-        const registerData = {
-            email: "example@mail.com",
-            password: "password"
-        }
-        const { statusCode, body } = await request(app).post("/api/users/register").send(registerData);
+    // test("test register with correct data", async () => {
+    //     const registerData = {
+    //         email: "example@mail.com",
+    //         password: "password"
+    //     }
+    //     const { statusCode, body } = await request(app).post("/api/users/register").send(registerData);
       
         
-        expect(statusCode).toBe(201);
-        expect(body.email).toBe(registerData.email);
-        expect(body.subscription).toBe("starter");
+    //     expect(statusCode).toBe(201);
+    //     expect(body.email).toBe(registerData.email);
+    //     expect(body.subscription).toBe("starter");
 
-        const user = await User.findOne({ email: registerData.email });
-        expect(user.email).toBe(registerData.email)
-    })
+    //     const user = await User.findOne({ email: registerData.email });
+    //     expect(user.email).toBe(registerData.email)
+    // })
 
     test('test login with correct data', async () => { 
         const loginData = {
@@ -43,12 +42,13 @@ describe("test login route", () => {
             password: "password"
         }
         const { statusCode, body } = await request(app).post("/api/users/login").send(loginData);
+        
 
         const user = await User.findOne({ email: loginData.email });
 
         expect(statusCode).toBe(200);
-        expect(body.email).toBe(loginData.email);
-        expect(body.subscription).toBe(user.subscription);
+        expect(body.user.email).toBe(loginData.email);
+        expect(body.user.subscription).toBe(user.subscription);
         expect(body.token).toBe(user.token);
     })
 })
